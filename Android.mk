2829dd7a3116a9741f8175a7ba07575d58c9f573
Konsta <konsta09@gmail.com>
2016-01-08 18:29:02 +0200

    wifi: add prebuilt pronto module

--- Android.mk
+++ Android.mk
@@ -21,10 +21,14 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Symlinks
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
-    ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
-    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
+    ln -sf /persist/wlan_mac.bin \
+    $(TARGET_OUT_ETC)/firmware/wlan/prima/wlan_mac.bin; \
     ln -sf /persist/WCNSS_wlan_dictionary.dat \
     $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
     $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+
+$(shell mkdir -p $(TARGET_OUT)/lib/modules/pronto; \
+    ln -sf /system/lib/modules/pronto/pronto_wlan.ko \
+    $(TARGET_OUT)/lib/modules/wlan.ko)
 endif
