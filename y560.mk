f8936e15d34d7847fcf06a06f66c8824a2c25282
Konsta <konsta09@gmail.com>
2016-01-21 19:41:03 +0200

    use xhdpi/1024 dalvik heap
    Change-Id: Ic0f3e3bacfac5ff1dcd80bfb93170e4cfa80de8c

--- y560.mk
+++ y560.mk
@@ -17,7 +17,7 @@
 DEVICE_PACKAGE_OVERLAYS += device/huawei/y560/overlay
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, frameworks/native/build/phone-hdpi-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/huawei/y560/y560-vendor.mk)
 
 PRODUCT_AAPT_CONFIG := normal
