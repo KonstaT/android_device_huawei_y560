KonstaT/android_device_zte_kis3

selinux: Drop BOARD_SEPOLICY_UNION
As suggested in the comments on
https://android-review.googlesource.com/#/c/141560/
drop BOARD_SEPOLICY_UNION and simplify the build_policy logic.
Union all files found under BOARD_SEPOLICY_DIRS.

Change-Id: I4214893c999c23631f5456cb1b8edd59771ef13b
Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

BoardConfig.mk
@@ -103,27 +103,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/zte/kis3/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    init_shell.te \
-    kernel.te \
-    location.te \
-    mediaserver.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    platform_app.te \
-    property.te \
-    property_contexts \
-    rmt_storage.te \
-    system_server.te \
-    thermal-engine.te \
-    wcnss_service.te
+BOARD_SEPOLICY_DIRS += device/zte/kis3/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true

KonstaT/android_device_huawei_y560

BoardConfig.mk
@@ -118,18 +118,7 @@ COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/huawei/y560/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file_contexts \
-    kernel.te \
-    mediaserver.te \
-    mm-qcamerad.te \
-    property.te \
-    property_contexts \
-    system_server.te
+BOARD_SEPOLICY_DIRS += device/huawei/y560/sepolicy
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun1/file