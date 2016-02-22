6b1460e33a8fa60a0a5c36aecc5028b98372ed03
Konsta <konsta09@gmail.com>
2016-02-22 19:20:03 +0200

    healthd: rename
    * Fixes the build after https://github.com/CyanogenMod/android_system_core/commit/4ca66c4d62bd1867520ee42c7077503ac8582c10
    
    Change-Id: Ib04bc9b362fd40f5fb5a30c4e347c8c563caae9f

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := healthd_board_msm.cpp
-LOCAL_MODULE := libhealthd.msm
+LOCAL_SRC_FILES := healthd_board_y560.cpp
+LOCAL_MODULE := libhealthd.y560
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
 include $(BUILD_STATIC_LIBRARY)
